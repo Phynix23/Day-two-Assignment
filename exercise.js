dde3d556ba9aa23bde6993d2dc8aa5ecbc842d54
Abuoma David <davidabuoma4@gmail.com>
2026-03-12 08:55:16 +0100

    init setup

---
+++ exercise.js
@@ -0,0 +1,57 @@
+
+// for (let table = 1; table <= 120; table++) {
+//   console.log(`${table}`);
+//   for (let multiplier = 1; multiplier <= 120; multiplier++) {
+//     console.log(`${table} x ${multiplier} = ${table * multiplier}`);
+//   }
+// }
+
+// for (let table = 1; table <= 120; table++) {
+//   console.log(`\nMultiplication table ${table}`);  // Added "Table" and a newline for better formatting
+//   for (let multiplier = 1; multiplier <= 120; multiplier++) {
+//     console.log(`${table} x ${multiplier} = ${table * multiplier}`);
+//   }
+// }
+
+// let baseNumber = 100;
+
+// for (let multiplier = 1; baseNumber <= 120; multiplier++) {
+//   if (multiplier === 1) {
+//     console.log(`\nTable ${baseNumber}`);
+//   }
+  
+//   console.log(`${baseNumber} x ${multiplier} = ${baseNumber * multiplier}`);
+  
+//   if (multiplier === 120) {
+//     multiplier = 0;
+//     baseNumber++;
+//   }
+// }
+
+// const baseNumber = 8
+
+// for (i=1; i<=120; i++){
+//   console.log(`${baseNumber} x ${i} = ${baseNumber * i}`)
+// }
+
+// const baseNumber = 9 
+
+// for (i=1; i<=120; i++){
+//   console.log(`${baseNumber} x ${i} ${baseNumber * i}`)
+// }
+
+// const baseNumber = 10;
+// for(let i=1; i<=120; i++){
+//   console.log()
+// }
+
+let baseNumber = 100
+
+for(let i=1;i<=120;i++){   
+     console.log(`${baseNumber} x ${i} = ${baseNumber*i}`)
+if(i==120 && baseNumber < 120){
+    i=0
+    baseNumber++
+    }
+
+}
